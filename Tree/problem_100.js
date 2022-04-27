759dd7d48349329f703761ba4daf0bf23045b1be
Michael Wu <michael80402@gmail.com>
2022-04-27 16:57:04 +0800

    add problem 100

---
+++ Tree/problem_100.js
@@ -0,0 +1,28 @@
+var isSameTree = function(p, q) {
+    if (!p && !q) {
+        return true;
+    }
+    if (!p || !q || p.val !== q.val) {
+        return false;
+    }
+
+    return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+};
+
+// class TreeNode {
+//     constructor (val, left, right) {
+//         this.val = val === undefined ? 0 : val;
+//         this.left = left === undefined ? null : left;
+//         this.right = right === undefined ? null : right;
+//     }
+// };
+// let n1 = new TreeNode(1);
+// let n2 = new TreeNode(2);
+// let n3 = new TreeNode(3);
+// let m1 = new TreeNode(1);
+// let m2 = new TreeNode(2);
+// let m3 = new TreeNode(3);
+// n1.left = n2;
+// n1.right = n3;
+// m1.left = m2;
+// m1.right = m3;
