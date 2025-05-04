ea6828691421bc5019d49b2e2fab5d6f82bdcd87
Michael.Wu <michael80402@gmail.com>
2025-05-04 16:06:13 +0800

    chore: resloved problem 9

--- Math/9.js
+++ Math/9.js
@@ -2,7 +2,7 @@
  * @param {number} x
  * @return {boolean}
  */
-var isPalindrome = function (x) {
+var isPalindrome1 = function (x) {
     if (x < 0) {
         return false;
     } else {
@@ -14,4 +14,33 @@ var isPalindrome = function (x) {
         }
         return true;
     }
+};
+
+/**
+ * 解法:
+ * 1. 如果是负数, 直接返回false
+ * 2. 將x除以10後取餘數，便可拿到最後一位(放入reverse)
+ * 3. 將x除以10後無條件進位，便可去掉最後一位(重新賦值給x)
+ * 4. 將reverse乘以10加上這次的餘數(便可得到新的reverse)
+ * 5. 重複2~4步驟，直到x完全沒有值(全部被去掉)
+ * 6. 比對x與reverse
+ */
+var isPalindrome2 = function (x) {
+    // 1.
+    if (x < 0) {
+        return false;
+    }
+
+    let reverse = 0;
+    let xcopy = x;
+
+    while (x > 0) {
+        // 2. & 4.
+        reverse = reverse * 10 + (x % 10);
+        // 3.
+        x = Math.floor(x / 10);
+    }
+
+    // 6.
+    return reverse === xcopy;
 };
